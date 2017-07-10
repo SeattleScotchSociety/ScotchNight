5c8538dcd25dfb58e300e08dd011882442858f1a
Travis Merkel <tmerkel@live.com>
2017-07-09 22:34:50 -0700

    Adding stores Adding location management

---
+++ src/SeattleScotchSociety.ScotchNight.Api/Controllers/LocationsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeattleScotchSociety.ScotchNight.Api.Data;
+using SeattleScotchSociety.ScotchNight.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SeattleScotchSociety.ScotchNight.Api.Filters;
+
+namespace SeattleScotchSociety.ScotchNight.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ValidateModel]
+    public class LocationsController : Controller
+    {
+        private ILocationStore _locationStore;
+
+        public LocationsController(ILocationStore locationStore)
+        {
+            _locationStore = locationStore;
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<Location>> GetAllAsync()
+        {
+            return _locationStore.GetAllAsync();
+        }
+
+        [HttpGet("{id}")]
+        public Bottle Get(Guid id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody]Location location)
+        {
+            await _locationStore.AddAsync(location);
+
+            return Accepted();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync([FromBody]Location location)
+        {
+            await _locationStore.UpdateAsync(location);
+
+            return Accepted();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody]Location location)
+        {
+            await _locationStore.DeleteAsync(location);
+
+            return Accepted();
+        }
+    }
+}
